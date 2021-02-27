df4c41a5f84ddc678cfbf50782d577df3c0f0c91
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-02-27 15:27:04 +0600

    space remove

--- delete-inline.php
+++ delete-inline.php
@@ -1,14 +1,8 @@
 <?php
  $stu_id = $_GET['id'];
-
         include'config.php';
         $sql = "DELETE FROM student WHERE sid ={$stu_id}";
         $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
-
-
         header("Location: http://localhost/PHP_CRUD/index.php");
-
         mysqli_close($conn);
-
-
 ?>
