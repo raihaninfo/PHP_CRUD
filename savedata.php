ca0b1d736d7326cc05eafd4e6c4f6aacd7b28d44
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-20 12:15:16 +0600

    space remove

--- savedata.php
+++ savedata.php
@@ -5,7 +5,6 @@
  $stu_class = $_POST['class'];
  $stu_phone = $_POST['sphone'];
 
- 
  include'config.php';
     $sql = "INSERT INTO student(sname, saddress, sclass, sphone) VALUES('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
     $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
